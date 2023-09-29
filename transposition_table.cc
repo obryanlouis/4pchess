2932dfc197bf3b060953fca41b39cc87edd33e03
qrealm <145950964+qrealm@users.noreply.github.com>
2023-09-29 11:20:14 -0500

    Refactor code.

--- transposition_table.cc
+++ transposition_table.cc
@@ -6,41 +6,37 @@
 
 namespace chess {
 
-TranspositionTable::TranspositionTable(size_t table_size) {
-  assert((table_size > 0) && "transposition table_size = 0");
-  table_size_ = table_size;
-  hash_table_ = (HashTableEntry*) calloc(table_size, sizeof(HashTableEntry));
-  assert(
-      (hash_table_ != nullptr) && 
-      "Can't create transposition table. Try using a smaller size.");
+const HashTableEntry* TranspositionTable::Get(uint64_t key) const {
+    size_t n = key % table_size_;
+    const HashTableEntry& entry = hash_table_[n];
+    if (entry.key == key && entry.generation == currentGeneration) {
+        return &entry;
+    }
+    return nullptr;
 }
 
-const HashTableEntry* TranspositionTable::Get(int64_t key) {
-  size_t n = key % table_size_;
-  HashTableEntry* entry = hash_table_ + n;
-  if (entry->key == key) {
-    return entry;
-  }
-  return nullptr;
+void TranspositionTable::Save(uint64_t key, int depth, std::optional<Move> move, int score, ScoreBound bound, int is_pv) {
+    size_t n = key % table_size_;
+    HashTableEntry& entry = hash_table_[n];
+    if (entry.key == 0 || entry.generation < currentGeneration ||
+       (entry.generation == currentGeneration && entry.depth < depth)) {
+        entry.key = key;
+        entry.depth = depth;
+        entry.move = move;
+        entry.score = score;
+        entry.bound = bound;
+        entry.is_pv = is_pv;
+        entry.generation = currentGeneration;
+    }
 }
 
-void TranspositionTable::Save(
-    int64_t key, int depth, std::optional<Move> move, int score,
-    ScoreBound bound, bool is_pv) {
-  size_t n = key % table_size_;
-  HashTableEntry& entry = hash_table_[n];
-  if (bound == EXACT
-      || entry.key != key
-      || entry.depth < depth) {
-    entry.key = key;
-    entry.depth = depth;
-    entry.move = move;
-    entry.score = score;
-    entry.bound = bound;
-    entry.is_pv = is_pv;
-  }
+void TranspositionTable::clear() {
+    std::fill(hash_table_.begin(), hash_table_.end(), HashTableEntry{});
 }
 
+void TranspositionTable::newSearch() {
+    currentGeneration++;
+}
 
 }  // namespace chess
 
